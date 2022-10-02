a3d619ba751a288e23c38afc361b68c18fe520e4
Carlos Toledo <carlombo@hotmail.com>
2022-10-02 15:59:26 -0300

    Add two images and text in home, add footer

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,26 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+
+window.sr = ScrollReveal();
+sr.reveal('#img01', {
+    duration: 2000,
+    delay: 800
+    });
+
+  window.sr = ScrollReveal();
+  sr.reveal('#img02', {
+      duration: 2000,
+      origin: 'left',
+      distance: '300px',
+      viewFactor: 0.2
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('#img03', {
+      duration: 2000,
+      origin: 'right',
+      distance: '300px',
+      viewFactor: 0.2
+  });
